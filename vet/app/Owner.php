0bbe5c4b1830266f3d3f19af1da5405e37d30043
Oliver Ballon <oliverballon1@gmail.com>
2020-08-12 09:47:05 +0100

    Updated challenges, unfinished methods for Owners

--- vet/app/Owner.php
+++ vet/app/Owner.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Owner extends Model
 {
 
-  
+        public static function fullName()
+        {
+            return $this->first_name." ".$this->last_name;
+        }
+
+        public static function fullAddress()
+        {
+            return Owner::$this->address_1." ".$this->address_2;
+        }
 }

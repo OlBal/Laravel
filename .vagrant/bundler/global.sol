71a6046259cd009a0e3988b94b1bd26ab61aa8ee
Oliver Ballon <oliverballon1@gmail.com>
2020-10-31 14:11:12 +0000

    vagrant added

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostsupdater",["= 1.1.1.160"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"cfedfa9ed989ca90a71c6ffa3a6ee80fc8462b23d1190ebdcd06f156db4e36ea","vagrant_version":"2.2.9"}
